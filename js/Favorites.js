04a50d13f5a753983800e37352ecb72731ccf337
lucas <ljanaudis@gmail.com>
2023-02-16 01:17:52 -0300

    JS and button improvements

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,3 +1,5 @@
+// class that will contain the logic
+// how data will be structured
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
@@ -5,23 +7,20 @@ export class Favorites {
   }
 
   load() {
-    this.entries = [
-      {
-        login: '/Lucasjs0',
-        name: 'Lucas Janaudis Soares',
-        public_repos: '21',
-        followers: '17'
-      },
-      {
-        login: '/diego3g',
-        name: 'Diego Fernandes',
-        public_repos: '30',
-        followers: '25000'
-      }
-    ]
+    this.entries = JSON.parse(localStorage.getItem('@gitfav:')) || []
+  }
+
+  delete(user) {
+    const filteredEntries = this.entries
+      .filter(entry => entry.login !== user.login)
+    
+    this.entries = filteredEntries
+    this.update()
+
   }
 }
 
+// class that will create the HTML view and events
 export class FavoritesView extends Favorites {
   constructor(root) {
     super(root)
@@ -34,7 +33,7 @@ export class FavoritesView extends Favorites {
   update() {
     this.removeAllTr()
 
-    this.entries.forEach( user => {
+    this.entries.forEach(user => {
       const row = this.createRow()
       
       row.querySelector('.user img').src = `https://github.com/${user.login}.png`
@@ -44,10 +43,15 @@ export class FavoritesView extends Favorites {
       row.querySelector('.repositories').textContent = user.public_repos
       row.querySelector('.followers').textContent = user.followers
 
+      row.querySelector('.remove').onclick = () => {
+        const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+        if(isOk) {
+          this.delete(user)
+        }
+      }
+
       this.tbody.append(row)
     })
-
-
   }
 
   createRow() {
